44b6a2be0dfef85323a6d0daa5f3a49c1ea04b26
ruizguille <ruiz.ruiz.guillermo@gmail.com>
2024-06-07 15:11:36 +0200

    Local assistant updates

--- backend/app/local_assistant.py
+++ backend/app/local_assistant.py
@@ -11,18 +11,36 @@ from deepgram import (
 from rich.console import Console
 from app.config import settings
 
-SYSTEM_PROMPT = """You are a helpful and enthusiastic conversational assistant.
-Your answers should be very short and concise as you will be having a voice conversation with the user.
+SYSTEM_PROMPT = """You are a helpful and enthusiastic assistant. Speak in a human, conversational tone.
+Keep your answers as short and concise as possible, like in a conversation, ideally no more than 120 characters.
 """
 
 DEEPGRAM_TTS_URL = 'https://api.deepgram.com/v1/speak?model=aura-luna-en&encoding=linear16&sample_rate=24000'
 
 console = Console()
 groq = AsyncGroq(api_key=settings.GROQ_API_KEY)
-deepgram_config: DeepgramClientOptions = DeepgramClientOptions(
-    options={'keepalive': 'true'}
+
+# Create the Deepgram client
+deepgram_config = DeepgramClientOptions(options={'keepalive': 'true'})
+deepgram = DeepgramClient(settings.DEEPGRAM_API_KEY, config=deepgram_config)
+
+# Configure Deepgram options for live transcription
+dg_connection_options = LiveOptions(
+    model='nova-2',
+    language='en',
+    # Apply smart formatting to the output
+    smart_format=True,
+    # Raw audio format details
+    encoding='linear16',
+    channels=1,
+    sample_rate=16000,
+    # To get UtteranceEnd, the following must be set:
+    interim_results=True,
+    utterance_end_ms='1500',
+    vad_events=True,
+    # Time in milliseconds of silence to wait for before finalizing speech
+    endpointing=500,
 )
-deepgram: DeepgramClient = DeepgramClient(settings.DEEPGRAM_API_KEY, config=deepgram_config)
 
 
 async def assistant_chat(messages, model='llama3-8b-8192'):
@@ -30,13 +48,14 @@ async def assistant_chat(messages, model='llama3-8b-8192'):
     return res.choices[0].message.content
 
 async def transcribe_audio():
+    transcript_parts = []
+    full_transcript = ''
     # Event to signal transcription is complete
     transcription_complete = asyncio.Event()
     
     try:
+        # Create a websocket connection to Deepgram
         dg_connection = deepgram.listen.asynclive.v('1')
-        transcript_parts = []
-        full_transcript = ''
 
         async def on_message(self, result, **kwargs):
             nonlocal transcript_parts, full_transcript
@@ -50,41 +69,28 @@ async def transcribe_audio():
                 
                 # Sufficent silence detected to consider this end of speech
                 if result.speech_final:
-                    transcription_complete.set()
                     full_transcript = ' '.join(transcript_parts)
+                    transcription_complete.set()
             else:
                 # Interim results
                 console.print(sentence, style='cyan', end='\r')
         
+        async def on_utterance_end(self, utterance_end, **kwargs):
+            nonlocal transcript_parts, full_transcript
+            if len(transcript_parts) > 0:
+                full_transcript = ' '.join(transcript_parts)
+                transcription_complete.set()
+        
         async def on_error(self, error, **kwargs):
             console.print(f'Error: {error}', style='red')
         
+        # Register the event handlers
         dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
+        dg_connection.on(LiveTranscriptionEvents.UtteranceEnd, on_utterance_end)
         dg_connection.on(LiveTranscriptionEvents.Error, on_error)
 
-        # connect to websocket
-        options: LiveOptions = LiveOptions(
-            model='nova-2',
-            language='en',
-            # Apply smart formatting to the output
-            smart_format=True,
-            # Raw audio format details
-            encoding='linear16',
-            channels=1,
-            sample_rate=16000,
-            # To get UtteranceEnd, the following must be set:
-            interim_results=True,
-            utterance_end_ms='1500',
-            vad_events=True,
-            # Time in milliseconds of silence to wait for before finalizing speech
-            endpointing=500,
-        )
-        addons = {
-            # Prevent waiting for additional numbers
-            'no_delay': 'true'
-        }
-
-        if await dg_connection.start(options, addons=addons) is False:
+        # Start the connection
+        if await dg_connection.start(dg_connection_options) is False:
             console.print('Failed to connect to Deepgram')
             return
         
@@ -96,6 +102,7 @@ async def transcribe_audio():
         # Wait for the transcription to complete
         await transcription_complete.wait()
         
+        # Close the microphone and the Deepgram connection
         microphone.finish()
         await dg_connection.finish()
         return full_transcript
@@ -132,7 +139,7 @@ def text_to_speech(text):
 
 async def run():
     system_message = {'role': 'system', 'content': SYSTEM_PROMPT}
-    memory_size = 8
+    memory_size = 10
     messages = []
     while True:
         user_message = await transcribe_audio()
@@ -141,7 +148,7 @@ async def run():
         if should_end_conversation(user_message):
             break
 
-        assistant_message = await assistant_chat([system_message] + messages[-memory_size:])
+        assistant_message = await assistant_chat([system_message] + messages[-memory_size+1:])
         messages.append({'role': 'assistant', 'content': assistant_message})
         console.print(assistant_message, style='dark_orange')
         text_to_speech(assistant_message)
